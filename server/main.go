ee9027d7fd4ab31737e8bdd94b1f2e5d9866d4ce
fenroe <scrambles9494@gmail.com>
2024-10-13 03:12:33 +0300

    Server running on standard port

--- server/main.go
+++ server/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hi banana!")
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: nil,
+	}
+	log.Printf("Serving on port: %s\n", ":8080")
+	log.Fatal(server.ListenAndServe())
 }
