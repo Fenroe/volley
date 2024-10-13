78a09bc79bf7f8c4ad8aa709bd1d6e43a7f69306
fenroe <scrambles9494@gmail.com>
2024-10-13 12:57:09 +0300

    Fixed gosec security issues

--- server/main.go
+++ server/main.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hi banana!")
 	server := http.Server{
-		Addr:    ":8080",
-		Handler: nil,
+		Addr:              ":8080",
+		Handler:           nil,
+		ReadHeaderTimeout: time.Duration(3) * time.Second,
 	}
 	log.Printf("Serving on port: %s\n", ":8080")
 	log.Fatal(server.ListenAndServe())
